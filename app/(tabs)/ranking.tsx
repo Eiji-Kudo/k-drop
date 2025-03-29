b2ac4898397d72f1d80df935bb26e31cef97fb7d
Eiji Kudo <outlook.best116@gmail.com>
2025-03-29 21:29:20 +0900

    feat(tabs): translate tab titles to Japanese

--- app/(tabs)/ranking.tsx
+++ app/(tabs)/ranking.tsx
@@ -20,7 +20,7 @@ export default function RankingScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
+        <ThemedText type="title">ランキング</ThemedText>
       </ThemedView>
       <ThemedText>This app includes example code to help you get started.</ThemedText>
       <Collapsible title="File-based routing">
