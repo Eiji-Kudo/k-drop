829fbf5afac8bde477475fa830f76cd073961e87
Eiji Kudo <outlook.best116@gmail.com>
2025-03-30 18:38:40 +0900

    refactor: simplify color management by removing useThemeColor hook and consolidating Colors structure

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,28 +1,11 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
 const tintColorLight = '#0a7ea4'
-const tintColorDark = '#fff'
 
 export const Colors = {
-  light: {
-    text: '#11181C',
-    background: '#FAF0F4',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
-    border: '#F0C2D9',
-  },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
-    border: '#F0C2D9',
-  },
+  text: '#11181C',
+  background: '#FAF0F4',
+  tint: tintColorLight,
+  icon: '#687076',
+  tabIconDefault: '#687076',
+  tabIconSelected: tintColorLight,
+  border: '#F0C2D9',
 }
