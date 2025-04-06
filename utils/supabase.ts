0e1a80e638eaf87a3cd5c95f4ea9b23fc1b8681b
Eiji Kudo <outlook.best116@gmail.com>
2025-04-06 21:21:05 +0900

    feat(supabase): validate environment variables for Supabase client initialization
    - Added a check to ensure Supabase URL and anon key are provided in environment variables.
    - Moved the import statement for 'react-native-url-polyfill/auto' to the correct position in the file.

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,9 +1,13 @@
-import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
+import 'react-native-url-polyfill/auto';
 
 const supabaseUrl = process.env.REACT_NATIVE_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_NATIVE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase URL and anon key must be provided in environment variables');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
