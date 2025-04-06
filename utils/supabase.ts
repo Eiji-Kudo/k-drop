165c12963da599996ee1f38962e53187ffd48442
Eiji Kudo <outlook.best116@gmail.com>
2025-04-06 22:36:53 +0900

    refactor(supabase): enforce type safety for Supabase configuration variables
    - Cast Supabase URL and anon key to string type for improved type safety.
    - Ensured consistency in handling environment variables for Supabase client initialization.

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -3,8 +3,10 @@ import { createClient } from '@supabase/supabase-js'
 import Constants from 'expo-constants'
 import 'react-native-url-polyfill/auto'
 
-const supabaseUrl = Constants.expoConfig?.extra?.supabaseUrl
-const supabaseAnonKey = Constants.expoConfig?.extra?.supabaseAnonKey
+const supabaseUrl = Constants.expoConfig?.extra?.supabaseUrl as string
+const supabaseAnonKey = Constants.expoConfig?.extra?.supabaseAnonKey as string
+
+
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Supabase URL and anon key must be provided in environment variables')
