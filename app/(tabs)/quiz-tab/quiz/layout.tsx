cbc944808511f4ca61effc2e5746755844058cf7
Eiji Kudo <outlook.best116@gmail.com>
2025-04-23 00:52:32 +0900

    feat(quiz): add QuizLayout component for quiz tab interface

---
+++ app/(tabs)/quiz-tab/quiz/layout.tsx
@@ -0,0 +1,19 @@
+import { Stack } from 'expo-router'
+import { StyleSheet, View } from 'react-native'
+
+import { Colors } from '@/constants/Colors'
+
+export default function QuizLayout() {
+  return (
+    <View style={styles.container}>
+      <Stack screenOptions={{ headerShown: false }} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.background,
+  },
+}) 
