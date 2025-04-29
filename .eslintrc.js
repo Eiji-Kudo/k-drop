e899c40ae78df6241dae7e93b97d99930c664536
Eiji Kudo <outlook.best116@gmail.com>
2025-04-29 20:44:40 +0900

    chore(lint): remove unnecessary TypeScript safety checks from ESLint configuration
    - Disabled '@typescript-eslint/no-unsafe-assignment' and '@typescript-eslint/no-unsafe-member-access' rules to allow for more flexibility in test files.
    - Streamlined ESLint settings to enhance developer experience while maintaining code quality.

--- .eslintrc.js
+++ .eslintrc.js
@@ -92,8 +92,6 @@ module.exports = {
         '@typescript-eslint/no-unsafe-return': 'off',
         '@typescript-eslint/no-unsafe-call': 'off',
         '@typescript-eslint/no-unsafe-argument': 'off',
-        '@typescript-eslint/no-unsafe-assignment': 'off',
-        '@typescript-eslint/no-unsafe-member-access': 'off',
         'max-lines': 'off',
       },
     },
