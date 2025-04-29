8cbd9f7a69bd78e2aa013b1a5f18a6776915f390
Eiji Kudo <outlook.best116@gmail.com>
2025-04-29 20:21:05 +0900

    lint

--- features/answer-quiz/hooks/__tests__/useSyncUnansweredQuizIds.test.tsx
+++ features/answer-quiz/hooks/__tests__/useSyncUnansweredQuizIds.test.tsx
@@ -1,9 +1,6 @@
 import { renderHook } from '@testing-library/react'
 import { useSyncUnansweredQuizIds } from '../useSyncUnansweredQuizIds'
-import {
-    mockRef,
-    setupQueryMocks
-} from './mocks/syncUnansweredQuizIdsMocks'
+import { mockRef, setupQueryMocks } from './mocks/syncUnansweredQuizIdsMocks'
 
 // Setup mocks
 jest.mock('@/context/GlobalContext')
@@ -24,70 +21,77 @@ jest.mock('react', () => {
   const originalReact = jest.requireActual('react')
   return {
     ...originalReact,
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     useRef: jest.fn().mockImplementation(() => mockRef),
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
     useEffect: jest.fn().mockImplementation((fn) => fn()),
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
     useMemo: jest.fn().mockImplementation((fn) => fn()),
   }
 })
 
 describe('useSyncUnansweredQuizIds', () => {
   const mockSetSelectedQuizIds = jest.fn()
-  
+
   beforeEach(() => {
     jest.clearAllMocks()
     mockRef.current = [99] // Reset to a value that will be different from empty array
   })
-  
+
   it('should not call setSelectedQuizIds when there is no context setter', () => {
     // Mock GlobalContext with undefined setSelectedQuizIds
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-argument
     setupQueryMocks(undefined as any, { withUserOnly: true })
-    
+
     renderHook(() => useSyncUnansweredQuizIds(1))
-    
+
     expect(mockSetSelectedQuizIds).not.toHaveBeenCalled()
   })
-  
+
   it('should set empty array when there are no unanswered quizzes', () => {
     // Setup with no quiz answers and no quizzes
-    setupQueryMocks(mockSetSelectedQuizIds, { withUserAnswers: false, withQuizzes: false })
-    
+    setupQueryMocks(mockSetSelectedQuizIds, {
+      withUserAnswers: false,
+      withQuizzes: false,
+    })
+
     renderHook(() => useSyncUnansweredQuizIds(1))
-    
+
     expect(mockSetSelectedQuizIds).toHaveBeenCalledWith([])
   })
-  
+
   it('should call setSelectedQuizIds with unanswered quiz IDs', () => {
     // Setup with default quiz answers and quizzes
     setupQueryMocks(mockSetSelectedQuizIds)
-    
+
     renderHook(() => useSyncUnansweredQuizIds(1))
-    
+
     // Should be called with quiz IDs 3 and 4 which are unanswered
     expect(mockSetSelectedQuizIds).toHaveBeenCalledWith([3, 4])
   })
-  
+
   it('should not update when unanswered quiz IDs remain the same', () => {
     // Set prevUnansweredRef to match what will be returned in this test
     mockRef.current = [2]
-    
+
     // Setup with limited quiz set and single answer
-    setupQueryMocks(mockSetSelectedQuizIds, { 
-      limitedQuizzes: true, 
-      singleAnswer: true 
+    setupQueryMocks(mockSetSelectedQuizIds, {
+      limitedQuizzes: true,
+      singleAnswer: true,
     })
-    
+
     // First render - should not update since prevUnansweredRef.current already equals [2]
     renderHook(() => useSyncUnansweredQuizIds(1))
-    
+
     // Since prevUnansweredRef.current already equals [2], it shouldn't call setSelectedQuizIds
     expect(mockSetSelectedQuizIds).not.toHaveBeenCalled()
-    
+
     // Now simulate a different prevUnansweredRef value to test it does update
     mockRef.current = [99]
-    
+
     // Re-render with same data but different prevUnansweredRef
     renderHook(() => useSyncUnansweredQuizIds(1))
-    
+
     // Now it should update
     expect(mockSetSelectedQuizIds).toHaveBeenCalledWith([2])
   })
